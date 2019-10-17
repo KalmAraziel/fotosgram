7ce8b03c9f891fd5e3cae79a1b46454f43ec6a76
Marcos Ahumada <marcos.ahumada@example.com>
2019-10-17 17:42:33 -0300

    componente avatar y eplementacion en login

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,11 +4,13 @@ import { PostComponent } from './post/post.component';
 import { PostsComponent } from './posts/posts.component';
 import { IonicModule } from '@ionic/angular';
 import { PipesModule } from '../pipes/pipes.module';
+import { AvatarSelectorComponent } from './avatar-selector/avatar-selector.component';
 
 @NgModule({
   declarations: [
     PostComponent,
-    PostsComponent
+    PostsComponent,
+    AvatarSelectorComponent
   ],
   imports: [
     CommonModule,
@@ -17,7 +19,8 @@ import { PipesModule } from '../pipes/pipes.module';
   ],
   exports: [
     PostComponent,
-    PostsComponent
+    PostsComponent,
+    AvatarSelectorComponent
   ]
 })
 export class ComponentsModule { }
