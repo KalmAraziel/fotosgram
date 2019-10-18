01181a1979e9662d7604d28c9bf383d5c09d8033
Marcos Ahumada <marcos.ahumada@example.com>
2019-10-18 15:55:37 -0300

    guard usuario y validacion de token

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Storage } from '@ionic/storage';
 import { environment } from '../../environments/environment';
 import { Usuario } from '../interfaces/interfaces';
+import { NavController } from '@ionic/angular';
 const URL = environment.url;
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+
   token: string = null;
+  usuario: Usuario = {};
+
   constructor(
     private http: HttpClient,
-    private storage: Storage
+    private storage: Storage,
+    private navCtrl: NavController
   ) { }
 
   login(email: string, password: string) {
@@ -45,8 +50,40 @@ export class UsuarioService {
       });
     });
   }
+
   async guardarToken(token: string) {
     this.token = token;
     await this.storage.set('token', token);
+  }
+
+  async cargarToken() {
+    this.token =  await this.storage.get('token') || null;
+  }
+
+  async validarToken(): Promise<boolean> {
+
+    await this.cargarToken();
+
+    if (!this.token) {
+      this.navCtrl.navigateRoot('/login', {animated: true });
+      return Promise.resolve(false);
+
+    }
+
+    return new Promise<boolean>( resolve => {
+      const headers = new HttpHeaders({
+        'x-token': this.token
+      });
+
+      this.http.get(`${ URL }/user/`, { headers } ).subscribe( (resp: any) => {
+        if (resp.ok) {
+          this.usuario = resp.usuario;
+          resolve(true);
+        } else {
+          this.navCtrl.navigateRoot('/login', {animated: true });
+          resolve(false);
+        }
+      });
+    });
   }
 }
